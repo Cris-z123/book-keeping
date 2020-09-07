2c893cbeb0611c872be318219f46aba428d05b12
ybZhang <472099249@qq.com>
2020-09-07 23:13:10 +0800

    EditLabel component

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        编辑
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import { Component } from 'vue-property-decorator';
+    import tagListModel from '@/models/tagListModel';
+
+    @Component
+    export default class EditLabel extends Vue {
+        created() {
+            const id = this.$route.params.id;
+            tagListModel.fetch();
+            const tags = tagListModel.data
+            const tag = tags.filter(t => t.id === id)[0]
+            if(tag) {
+                console.log(tag);
+            } else {
+                this.$router.replace('/404');
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
