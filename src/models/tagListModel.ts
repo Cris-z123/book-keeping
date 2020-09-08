e7dfbe444764ce10e7a4069d8ee3f86c69057172
ybZhang <472099249@qq.com>
2020-09-08 21:13:00 +0800

    id生成器 完善label删除UI

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 type Tag = {
     id: string;
@@ -20,7 +22,8 @@ const tagListModel: TagListModel= {
     create(name: string) {
         const names = this.data.map(item => item.name)
         if(names.indexOf(name) >= 0) { return 'duplicated'; }
-        this.data.push({id: name, name: name});
+        const id = createId().toString();
+        this.data.push({id, name: name});
         this.save();
         return 'success';
     },
